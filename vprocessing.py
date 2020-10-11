1c2b420a13ec4f3e00e04cac1be3032e2022a416
João Barboza Rodrigues <joaobarboza.ufu@gmail.com>
2020-10-11 16:38:35 -0300

    ADD FILES VIA UPLOAD
    É troll , foi upload não

--- vprocessing.py
+++ vprocessing.py
@@ -12,54 +12,91 @@ from kernels import kernel
 # -------------------------INTERFACE------------------------- #
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        
-        self.effects_list = tk.Listbox(self)
-        self.effects_list.pack(side="left", fill=tk.BOTH, expand=1)
-        
-        self.video = tk.Canvas(self)
-        self.video.pack(side="right", fill=tk.BOTH, expand=1)
-
-
-root = tk.Tk()
-root.title("Video Processing")
-app = Application(master=root)
-# app.mainloop()
-
-
 # ------------------------CONVOLUTION------------------------ #
 
+
 def convolve(array_a, array_b):
-    fft_array_a = pyfftw.builders.fft2(array_a, threads=os.cpu_count())
-    fft_array_b = pyfftw.builders.fft2(array_b, s=array_a.shape,
-                                       threads=os.cpu_count())
-    ifft = pyfftw.builders.ifft2(fft_array_a()*fft_array_b(),
-                                 threads=os.cpu_count())
-    return np.real(ifft())[1:, 1:]
+    fft_array_a = pyfftw.builders.fft2(
+        array_a, threads=os.cpu_count())
+    fft_array_b = pyfftw.builders.fft2(
+        array_b, s=array_a.shape, threads=os.cpu_count())
+    ifft = pyfftw.builders.ifft2(
+        fft_array_a()*fft_array_b(), threads=os.cpu_count())
+    return np.real(ifft())
 
 
-# -------------------------VIDEO I/O------------------------- #
+# -------------------------INTERFACE------------------------- #
 
 
-cam = cv2.VideoCapture(0)
-if (not cam.isOpened()):
-    print("Error opening the camera")
-while(cam.isOpened()):
-    ret, frame = cam.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        conv = np.uint8(np.round(convolve(gray, kernel['sharpen'])))
-        cv2.imshow('Frame', conv)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-    else:
-        break
-cam.release()
-cv2.destroyAllWindows()
+class Application(tk.Frame):
+    def __init__(self):
+        self.root = tk.Tk()
+        super().__init__(self.root)
+        self.stream = None
+        self.video_panel = None
+        self.frame = None
+        self.effect = 0
+        self.pack()
+        self.create_widgets()
+        self.init_screen()
+        self.init_video_thread()
+
+    def init_screen(self):
+        self.root.wm_title("Video Processing")
+        self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+
+    def init_video_thread(self):
+        self.stopEvent = threading.Event()
+        self.thread = threading.Thread(target=self.video_loop)
+        self.thread.start()
+
+    def create_widgets(self):
+        self.effects_list = ttk.Combobox(self,
+                                         values=list(map(lambda n: n, kernel)),
+                                         state="readonly")
+        self.effects_list.pack(side="top", fill=tk.BOTH, expand=1)
+        self.effects_list.bind("<<ComboboxSelected>>", self.set_effect)
+
+    def set_effect(self, event):
+        self.effect = self.effects_list.current()
+
+    def video_loop(self):
+        self.stream = cv2.VideoCapture(0)
+        start = time.time()
+        while not self.stopEvent.is_set():
+            ret, self.frame = self.stream.read()
+            if ret:
+                self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2YCR_CB)
+                channels_list = []
+                convolve_result = self.channel_apply_effect(0)
+                image = cv2.merge((convolve_result, self.frame[:, :, 1], self.frame[:, :, 2]))
+                image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2RGB)
+                image = ImageTk.PhotoImage(Image.fromarray(image))
+                self.update_image(image)
+                print("FPS...", (1 / (time.time() - start)))
+                start = time.time()
+            else:
+                break
+        self.stream.release()
+
+    def update_image(self, image):
+        if self.video_panel is None:
+            self.video_panel = tk.Label(image=image)
+            self.video_panel.image = image
+            self.video_panel.pack(
+                side="bottom", fill=tk.BOTH, expand=1)
+        else:
+            self.video_panel.configure(image=image)
+            self.video_panel.image = image
+
+    def channel_apply_effect(self,i):
+        return np.uint8(np.round(convolve(
+            self.frame[:, :, i], kernel.get(list(kernel)[self.effect]))))
+
+    def onClose(self):
+        self.stopEvent.set()
+        self.root.quit()
+
+
+app = Application()
+app.mainloop()
